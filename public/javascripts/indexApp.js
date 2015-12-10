5538045ad27ed008a4abb1ccb6c71f0679e5b3f4
appcoreopc <kepung@gmail.com>
2015-12-10 18:47:32 +0800

    get catalog using angular.

--- public/javascripts/indexApp.js
+++ public/javascripts/indexApp.js
@@ -1,11 +1,23 @@
-var myApp = angular.module('myApp',[]);
-myApp.controller('OrderingController', ['$scope', function($scope) {
+var catalogApp = angular.module('catalogApp',[]).controller('catalogController', ['$scope', '$http', function($scope, $http) {
 
+ $scope.catalog = null;
+ var catalogUrl = '/catalog';
+
+ $scope.init = function()
+ {
+    $http({
+        url: catalogUrl,
+        method: "GET"
+     }).then(function(response){
+
+        if (response)
+        {
+            $scope.catalog = response.data[0];
+            console.log($scope.catalog);
+        }
+     }, function(errorResponse){
+
+     });
+ }
 }]);
 
-var init = function()
-{
-    console.log('ready!');
-}
-
-init();
